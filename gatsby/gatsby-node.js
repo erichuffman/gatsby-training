2e2034a623667979161b78c47f893165b888db6b
Grayson Hicks <graysonhicks@gmail.com>
2020-02-05 16:52:02 -0500

    MCTRAIN-100 -- document createPage function w comments

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -6,9 +6,14 @@
 
 const path = require('path');
 exports.createPages = async ({ graphql, actions }) => {
+  // createPages (note the plural) is a Gatsby API lifecycle hook.
+  // Each lifecycle hook receives an `actions` object with helper methods.
+  // We will be leveraging the `createPage` function.
   const { createPage } = actions;
   // **Note:** The graphql function call returns a Promise
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  // Here we query all of our movies and get their url (path.alias).
+  // Note we rename allNodeMovie to 'movies'
   const result = await graphql(`
     query {
       movies: allNodeMovie {
@@ -23,9 +28,14 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `);
 
+  // We then map over each movie in the array.
   result.data.movies.edges.forEach(({ node }) => {
+    // Now we call the createPage helper function and pass it
+    // the necessary data.
     createPage({
+      // The relative url we want for the page when published.
       path: node.path.alias,
+      // The path to the template component for this page.
       component: path.resolve('./src/templates/movie.js'),
       context: {
         // Data passed to context is available
